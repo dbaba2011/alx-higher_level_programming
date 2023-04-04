4c3d8b489ba186c272a3d693928158bee37f8a1f
dbaba2011 <dbaba2011@yahoo.com.com>
2023-04-04 10:45:12 -0700

    a class Rectangle that defines a rectangle by (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -31,3 +31,21 @@ class Rectangle:
     def height(self):
         """Set the height of the Rectangle."""
         return self.__height
+
+     @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
