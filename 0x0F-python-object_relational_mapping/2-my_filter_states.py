504be6a50e8188a170babcb14fb9a70cbf5715e5
dbaba2011 <dbaba2011@yahoo.com.com>
2023-06-20 02:25:28 -0700

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
+""" This script takes in an argument and
 displays all values in the states
 where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
 """
+
+import MySQLdb
+from sys import argv
 if __name__ == "__main__":
-    """
-    This connect to the database and execute all queries it designs to perform
-    """
+    name = argv[4]
 
-    import MySQLdb
-    from sys import argv
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        user=f"{argv[1]}",
-        password=f"{argv[2]}",
-        db=f"{argv[3]}",
-        port=3306,
-    )
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           password=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name = '{}' \
-                ORDER BY states.id ASC".format(argv[4])
-    )
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    cur.execute("SELECT * FROM states WHERE name ='{:s}' ORDER BY id ASC"
+                .format(name,))
+    result = cur.fetchall()
+    for row in result:
+        if row[1] == name:
+            print(row)
+    cur.close
+    conn.close
