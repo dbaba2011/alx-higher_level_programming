efc1369151846d3b3dd9af5519a90441a87bbd2d
dbaba2011 <dbaba2011@yahoo.com.com>
2023-06-20 02:33:34 -0700

    a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""
-This script that lists all cities from the database hbtn_0e_4_usa
-"""
+""" Lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-    """
-    This connect to the database and execute all queries it designs to perform
-    """
-
-    import MySQLdb
-    from sys import argv
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        user=f"{argv[1]}",
-        password=f"{argv[2]}",
-        db=f"{argv[3]}",
-        port=3306,
-    )
-
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           password=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute(
-        "SELECT cities.id ,cities.name, states.name from cities inner join states on state_id = states.id ORDER BY cities.id ASC"
-    )
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states where states.id=cities.state_id")
+    results = cur.fetchall()
+    for row in results:
         print(row)
-    cur.close()
-    conn.close()
+    cur.close
+    conn.close
