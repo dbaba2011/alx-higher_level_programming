bbd9d97930ff7b13cebdbe23e01af472be131c62
dbaba2011 <dbaba2011@yahoo.com.com>
2023-06-22 05:04:48 -0700

    a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-This script lists the first State objects
-found from the database `hbtn_0e_6_usa`.
-"""
-
+""" Select the first item in the table """
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
 from sys import argv
 
+if __name__ == "__main__"
+    engine = create_egine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           rgv[2], argv[3], pool_pre_ping=True))
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True,
-    )
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    result = session.query(State).order_by(State.id).first()
-    if result is not None:
-        print(f"{result.id}: {result.name}")
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
+    session.close()
